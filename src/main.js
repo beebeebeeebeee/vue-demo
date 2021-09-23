1fd2b312a035b1535a9d79bbe1730b36e43ccbae
Mak Chun Fung <chunfung_mak@RINGUS.LOCAL>
2021-09-23 17:34:19 +0800

    added pwa

--- src/main.js
+++ src/main.js
@@ -8,8 +8,11 @@ import App from './App.vue'
 // import store (for storing state)
 // @/sth the @/ is mena src/
 import store from "@/store"
+
 // import the ui plugin that you like (or other plugins)
 import naive from 'naive-ui'
+
+import './registerServiceWorker'
 
 // set app by createApp (I don't know how to explain)
 const app = createApp(App)
