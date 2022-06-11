46b8ace68d9739c73e04ee79154462e88afcce46
Ringus\chunfung_mak <chunfung.mak@ringus-solution.com>
2022-06-11 23:47:29 +0800

    deploy

---
+++ precache-manifest.5171e7f0357f56f62db9a2b198021c03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21e9c10c7290939d866",
+    "url": "/vue-demo/css/app.ce621b55.css"
+  },
+  {
+    "revision": "c6b3bcf57f86e4da822a",
+    "url": "/vue-demo/css/chunk-vendors.60240809.css"
+  },
+  {
+    "revision": "abe71f7d608d43b56d9b2aef78d7ae99",
+    "url": "/vue-demo/fonts/element-icons.abe71f7d.ttf"
+  },
+  {
+    "revision": "d9491be2c5109fca0fa40d0c59e2e3b9",
+    "url": "/vue-demo/fonts/element-icons.d9491be2.woff"
+  },
+  {
+    "revision": "738d7fc7a4129efa27d4f2054bb61e9a",
+    "url": "/vue-demo/index.html"
+  },
+  {
+    "revision": "b21e9c10c7290939d866",
+    "url": "/vue-demo/js/app.07afa9b9.js"
+  },
+  {
+    "revision": "c6b3bcf57f86e4da822a",
+    "url": "/vue-demo/js/chunk-vendors.11a983aa.js"
+  },
+  {
+    "revision": "6dfae02219e2e3140076978e5f08c0ad",
+    "url": "/vue-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-demo/robots.txt"
+  }
+]);
